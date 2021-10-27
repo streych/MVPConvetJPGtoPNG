65a7479c4444abe378974b1a86e981131821be9e
V radiulov <radiulovv.a@gmail.com>
2021-10-27 20:56:17 +1000

    Create project

---
+++ app/src/main/java/com/example/mvp_convet_gjp_png/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mvp_convet_gjp_png
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

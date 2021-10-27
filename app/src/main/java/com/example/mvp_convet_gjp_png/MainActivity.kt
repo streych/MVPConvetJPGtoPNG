2cbb093a2aa60f45a9e784408f864a5120d8c6a7
V radiulov <radiulovv.a@gmail.com>
2021-10-27 21:12:46 +1000

    MainActivity binding

--- app/src/main/java/com/example/mvp_convet_gjp_png/MainActivity.kt
+++ app/src/main/java/com/example/mvp_convet_gjp_png/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.mvp_convet_gjp_png
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mvp_convet_gjp_png.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
